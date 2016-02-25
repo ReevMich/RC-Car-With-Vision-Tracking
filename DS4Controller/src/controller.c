9f2d85d2623d3f776070b84f0e8e82dc8bf936c5
Michael Reeves <reevmich@my.dom.edu>
2016-02-24 18:11:13 -0600

    Fixed a bug where, the thumb sticks werent calculating the values correctly.

--- DS4Controller/src/controller.c
+++ DS4Controller/src/controller.c
@@ -111,7 +111,15 @@ bool getKeyDown (int button){
 }
 
 bool getAxisDown (int axis){
-  if(state.axis[axis] > 0){
+
+  if(axis == AXIS_LEFT_STICK_X || axis == AXIS_LEFT_STICK_Y ||
+     axis == AXIS_RIGHT_STICK_X || axis == AXIS_RIGHT_STICK_Y){
+
+    if((state.axis[axis] >= 0 || state.axis[axis] <= 100) && state.axis[axis] != 50){
+      return true;
+    }
+    
+  } else if(state.axis[axis] > 0){
     return true;
   }
   
--- DS4Controller/src/test.py
+++ DS4Controller/src/test.py
@@ -10,8 +10,17 @@ while(controller.active):
     if(ControllerModule.getAxisDown(ControllerModule.AXIS_R2)):
         print("R2=%d" % ControllerModule.getAxisValue(ControllerModule.AXIS_R2))
 
+    if(ControllerModule.getAxisDown(ControllerModule.AXIS_L2)):
+        print("L2=%d" % ControllerModule.getAxisValue(ControllerModule.AXIS_L2))
+
     if(ControllerModule.getAxisDown(ControllerModule.AXIS_LEFT_STICK_X)):
         print("LS_X=%d" % ControllerModule.getAxisValue(ControllerModule.AXIS_LEFT_STICK_X))
 
     if(ControllerModule.getAxisDown(ControllerModule.AXIS_LEFT_STICK_Y)):
         print("LS_Y=%d" % ControllerModule.getAxisValue(ControllerModule.AXIS_LEFT_STICK_Y))
+
+    if(ControllerModule.getAxisDown(ControllerModule.AXIS_RIGHT_STICK_X)):
+        print("RS_X=%d" % ControllerModule.getAxisValue(ControllerModule.AXIS_RIGHT_STICK_Y))
+
+    if(ControllerModule.getAxisDown(ControllerModule.AXIS_RIGHT_STICK_Y)):
+        print("RS_Y=%d" % ControllerModule.getAxisValue(ControllerModule.AXIS_RIGHT_STICK_Y))
