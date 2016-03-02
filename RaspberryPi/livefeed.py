4f6dfd93f49c95d5b3582b5356c70a35c3862882
Jesus Diaz <diazjesu@my.dom.edu>
2016-03-02 17:27:39 +0000

    Made livefeed.py a copy of only the simplecv code from MainDriver for testing

--- RaspberryPi/livefeed.py
+++ RaspberryPi/livefeed.py
@@ -1,12 +1,58 @@
-from SimpleCV import Camera
+#!/usr/bin/python
 
-# Basic setup for simpleCV to show a live feed
+import serial
+import SimpleCV
+from Queue import Queue
+from threading import Thread, Event
+from Queue import Queue
 
-cam = Camera()
+def main():
+        cam = SimpleCV.Camera()
+        image_queue = Queue()
 
-while True:
-	img = cam.getImage()
-	img = img.binarize()
-	img.drawText("Hello World")
-	img.show()
+        camera_thread = ImageCaptureThread(cam, image_queue)
+        img_display_thread = ImageDisplayThread(image_queue)
+        camera_thread.start()
+        img_display_thread.start()
 
+
+class ImageCaptureThread(Thread):
+        def __init__(self, camera, img_queue):
+                Thread.__init__(self)
+                self.cam = camera
+                self.img_queue = img_queue
+                self.thread_kill_request = Event()
+
+        def run(self):
+                while not self.thread_kill_request.is_set():
+                        try:
+                                img = cam.getImage()
+                                self.img_queue.put(img)
+                        except self.img_queue.Empty:
+                                continue
+
+        def join(self, timeout=None):
+                self.thread_kill_request.set()
+                super(ImageCaptureThread, self).join(timeout)
+
+class ImageDisplayThread(Thread):
+        def __init__(self, img_queue):
+                Thread.__init__(self)
+                self.img_queue = img_queue
+                self.thread_kill_request = Event()
+
+        def run(self):
+                while not self.thread_kill_request.is_set():
+                        try:
+                                img = self.img_queue.get()
+                                img.show()
+                        except self.img_queue.Empty:
+                                continue
+
+        def join(self, timeout=None):
+                self.thread_kill_request.set()
+                super(ImageDisplayThread, self).join(timeout)
+
+
+if __name__ == '__main__':
+        main()
