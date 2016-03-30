c7506f31addc4e25042e3a30774886458eb7ef05
dassjosh <dassjosh@my.dom.edu>
2016-03-30 18:10:44 -0500

    hopefully final fix

--- RaspberryPi/Arduino.py
+++ RaspberryPi/Arduino.py
@@ -13,7 +13,7 @@ RIGHT_FORWARD = None
 RIGHT_BACKWARD = None
 
 
-def main(arduino_wheel_speeds_queue):
+def main(arduino_wheel_speeds_queue, run_prog):
     global ARDUINO
     global LEFT_BACKWARD
     global LEFT_FORWARD
@@ -58,6 +58,11 @@ def main(arduino_wheel_speeds_queue):
             set_left_wheels(left_wheel)
             set_right_wheels(right_wheel)
         except arduino_wheel_speeds_queue.empty():
+            pass
+
+        try:
+            program_running = run_prog.get()
+        except run_prog.empty():
             pass
 
         sleep(.1)
--- RaspberryPi/Main_Driver.py
+++ RaspberryPi/Main_Driver.py
@@ -13,22 +13,24 @@ def main():
 
     run_prog = True
 
-    run_prog_queue = Queue()
-
+    run_prog_queue_cntrlr = Queue()
+    run_prog_queue_arduino = Queue()
     controller_queue = Queue()
     controller_ds4_proc = Process(target=DS4_Controller.main,
-                                  args=(controller_queue, run_prog_queue))
+                                  args=(controller_queue, run_prog_queue_cntrlr))
 
-    arduino_proc = Process(target=Arduino.main, args=(controller_queue,))
+    arduino_proc = Process(target=Arduino.main, args=(controller_queue,
+                                                      run_prog_queue_arduino))
 
     controller_ds4_proc.start()
     arduino_proc.start()
 
     while run_prog:
         try:
-            run_prog = run_prog_queue.get()
+            run_prog = run_prog_queue_cntrlr.get()
+            run_prog_queue_arduino.put(run_prog)
             print "Program Should terminate:" + str(run_prog)
-        except run_prog_queue.empty():
+        except run_prog_queue_cntrlr.empty():
             pass
 
         sleep(1)
