000cedea30562843e0c0647b28c023bfd99cae2d
dassjosh <dassjosh@my.dom.edu>
2016-04-03 19:11:27 -0500

    code cleanup

--- RaspberryPi/Arduino.py
+++ RaspberryPi/Arduino.py
@@ -8,18 +8,44 @@ LEFT_BACKWARD = 4
 RIGHT_FORWARD = 27
 RIGHT_BACKWARD = 22
 
-DISTANCE_SENSOR_TRIGGERED = False
-
 
 def main(arduino_wheel_speeds_pipe, dist_sensor_pipe):
 
-    global DISTANCE_SENSOR_TRIGGERED
-
     program_running = True
+    distance_sensor_triggered = False
 
     _, in_wheel_speed_pipe = arduino_wheel_speeds_pipe
     _, in_dist_sensor_pipe = dist_sensor_pipe
 
+    setup_gpio()
+
+    left_wheel, right_wheel, prev_left, prev_right = (0, 0, 0, 0)
+
+    while program_running:
+
+        if in_dist_sensor_pipe.poll():
+            distance_sensor_triggered = in_dist_sensor_pipe.recv()
+            print distance_sensor_triggered
+
+        if in_wheel_speed_pipe.poll():
+            prev_left, prev_right = left_wheel, right_wheel
+            left_wheel, right_wheel = in_wheel_speed_pipe.recv()
+
+        if distance_sensor_triggered is False:
+            if prev_left != left_wheel or prev_right != right_wheel:
+                print "Arduino Left: %.2f Right: %.2f" % \
+                      (left_wheel, right_wheel)
+                set_wheel_speeds(left_wheel, LEFT_FORWARD, LEFT_BACKWARD)
+                set_wheel_speeds(left_wheel, RIGHT_FORWARD, RIGHT_BACKWARD)
+        else:
+            print "Triggered Arduino Left: %.2f Right: %.2f" % (-0.3, -0.3)
+            set_wheel_speeds(-30, LEFT_FORWARD, LEFT_BACKWARD)
+            set_wheel_speeds(-30, RIGHT_FORWARD, RIGHT_BACKWARD)
+
+        sleep(.09)
+
+
+def setup_gpio():
     wiringpi.wiringPiSetupGpio()
 
     wiringpi.pinMode(LEFT_FORWARD, 1)
@@ -32,59 +58,20 @@ def main(arduino_wheel_speeds_pipe, dist_sensor_pipe):
     wiringpi.softPwmCreate(RIGHT_FORWARD, 0, 100)
     wiringpi.softPwmCreate(RIGHT_BACKWARD, 0, 100)
 
-    left_wheel, right_wheel, prev_left, prev_right = (0, 0, 0, 0)
 
-    while program_running:
+def set_wheel_speeds(speed, forward, backward):
+    abs_speed = int(abs(speed))
 
-        if in_dist_sensor_pipe.poll():
-            DISTANCE_SENSOR_TRIGGERED = in_dist_sensor_pipe.recv()
-        print DISTANCE_SENSOR_TRIGGERED
-
-        if in_wheel_speed_pipe.poll():
-            prev_left, prev_right = left_wheel, right_wheel
-            left_wheel, right_wheel = in_wheel_speed_pipe.recv()
-        if DISTANCE_SENSOR_TRIGGERED is False:
-            if prev_left != left_wheel or prev_right != right_wheel:
-                print "Arduino Left: %.2f Right: %.2f" % \
-                      (left_wheel, right_wheel)
-                set_left_wheels(left_wheel)
-                set_right_wheels(right_wheel)
-        else:
-            print "Triggered Arduino Left: %.2f Right: %.2f" % (-0.3, -0.3)
-            set_left_wheels(-30)
-            set_right_wheels(-30)
-
-        sleep(.05)
-
-
-def set_left_wheels(left):
-    abs_speed = abs(int(left))
-   
     if abs_speed < 10 and abs_speed != 0:
         abs_speed = 10
     elif abs_speed > 90:
         abs_speed = 90
 
-    if left >= 0:
-        wiringpi.softPwmWrite(LEFT_FORWARD, abs_speed)
-        wiringpi.softPwmWrite(LEFT_BACKWARD, 0)
+    if speed >= 0:
+        wiringpi.softPwmWrite(forward, abs_speed)
+        wiringpi.softPwmWrite(backward, 0)
     else:
         print "BackWard %d" % abs_speed
-        wiringpi.softPwmWrite(LEFT_FORWARD, 0)
-        wiringpi.softPwmWrite(LEFT_BACKWARD, abs_speed)
+        wiringpi.softPwmWrite(forward, 0)
+        wiringpi.softPwmWrite(backward, abs_speed)
 
-
-def set_right_wheels(right):
-    abs_speed = abs(int(right))
-
-    if abs_speed < 10 and abs_speed != 0:
-        abs_speed = 10
-    elif abs_speed > 90:
-        abs_speed = 90
-
-    if right >= 0:
-        wiringpi.softPwmWrite(RIGHT_FORWARD, abs_speed)
-        wiringpi.softPwmWrite(RIGHT_BACKWARD, 0)
-    else:
-        wiringpi.softPwmWrite(RIGHT_FORWARD, 0)
-        wiringpi.softPwmWrite(RIGHT_BACKWARD, abs_speed)
--- RaspberryPi/DS4_Controller.py
+++ RaspberryPi/DS4_Controller.py
@@ -34,19 +34,19 @@ def main(out_arduino_wheel_speed_pipe, out_run_prog_pipe):
             if controller.getAxisDown(controller.AXIS_R2):
                 right_wheels = controller.getAxisValue(controller.AXIS_R2)
             else:
-                right_wheels = 0.0
+                right_wheels = 0
             if controller.getAxisDown(controller.AXIS_L2):
                 left_wheels = controller.getAxisValue(controller.AXIS_L2)
             else:
-                left_wheels = 0.0
+                left_wheels = 0
 
             if reverse is True:
                 left_wheels = -left_wheels
                 right_wheels = -right_wheels
 
         except ValueError:
-            left_wheels = 0.0
-            right_wheels = 0.0
+            left_wheels = 0
+            right_wheels = 0
 
         if right_wheels != prev_right or left_wheels != prev_left:
             out_wheels_pipe.send((left_wheels * 100, right_wheels * 100))
