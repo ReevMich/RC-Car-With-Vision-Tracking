366fbbbc6068071387d21e9aa3f7d98e7a0358c5
Michael Reeves <reevmich@my.dom.edu>
2016-04-24 20:40:32 +0000

    Added new vision tracking ball search algorithm

--- RaspberryPi/BallTrackerHue.py
+++ RaspberryPi/BallTrackerHue.py
@@ -3,7 +3,7 @@
 import SimpleCV
 from time import sleep
 
-SPEED_MULTIPLIER = 5  # MAX 10
+SPEED_MULTIPLIER = 3  # MAX 10
 test = False
 POWER = 8 * SPEED_MULTIPLIER + 10
 
@@ -19,6 +19,10 @@ def main(out_wheels_pipe):
     center_height = img.height/2.0
     percentage_middle = center_width * .05
 
+    print "Center Width: %.2f" % center_width
+    print "Center Height: %.2f" % center_height
+    print "Percentage Middle: %.2f" % percentage_middle
+    
     if not test:
         out_wheels, _ = out_wheels_pipe
 
@@ -40,22 +44,28 @@ def main(out_wheels_pipe):
 
                 if x < (center_width - percentage_middle):
                     x_percentage = float(x) / (center_width - percentage_middle)
-                    x_percentage = abs(x_percentage - 100)
-                    left_wheel = -x_percentage * POWER
-                    right_wheel = x_percentage * POWER
-                elif x > (center_width + percentage_middle):
-                    x_percentage = float(x) / (center_width + percentage_middle)
-                    x_percentage = abs(x_percentage - 200)
+                    print "LEFT: NON ABS: x_percentage %.2f" % x_percentage
+                    x_percentage = -1.0*(x_percentage - 1)
+                    print "LEFT: ABS: x_percentage %.2f" % x_percentage
                     left_wheel = x_percentage * POWER
                     right_wheel = -x_percentage * POWER
+                elif x > (center_width + percentage_middle):
+                    x_percentage = float(x) / (center_width + percentage_middle)
+                    print "RIGHT: NON ABS: x_percentage %.2f" % x_percentage
+                    x_percentage = -1.0*(x_percentage - 2)
+                    print "RIGHT: ABS: x_percentage %.2f" % x_percentage
+                    left_wheel = -x_percentage * POWER
+                    right_wheel = x_percentage * POWER
                 else:
                     left_wheel = POWER
                     right_wheel = POWER
-
-                print "Left %f Right $f" % (left_wheel, right_wheel)
+                
+                #print "Left %f Right $f" % (left_wheel, right_wheel)
+                print left_wheel, right_wheel
                 out_wheels.send((left_wheel, right_wheel))
-        else:
-            out_wheels.send((0, 0))
+            else:
+                out_wheels.send((0, 0))
+                print "NO BLOBS"
 
 if __name__ == "__main__":
     main(None)
