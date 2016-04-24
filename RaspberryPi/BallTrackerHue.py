5ec1603f175cbf792a4f040803d8c082e0fd8789
dassjosh <dassjosh@my.dom.edu>
2016-04-24 13:51:28 -0500

    Ball Movinng rework

--- RaspberryPi/BallTrackerHue.py
+++ RaspberryPi/BallTrackerHue.py
@@ -5,12 +5,19 @@ from time import sleep
 
 SPEED_MULTIPLIER = 5  # MAX 10
 test = False
+POWER = 8 * SPEED_MULTIPLIER + 10
 
 
 def main(out_wheels_pipe):
 
     #display = SimpleCV.Display()
     cam = SimpleCV.Camera()
+
+    img = cam.getImage()
+
+    center_width = img.width/2.0
+    center_height = img.height/2.0
+    percentage_middle = center_width * .05
 
     if not test:
         out_wheels, _ = out_wheels_pipe
@@ -29,60 +36,26 @@ def main(out_wheels_pipe):
             if circles:
                 x = circles[-1].x
                 y = circles[-1].y
-                img.drawCircle((x, y), circles[-1].radius(), SimpleCV.Color.BLUE, 3)
-                
-                if not test:
-                    speed, turn_speed = (0, 0)
+                #img.drawCircle((x, y), circles[-1].radius(), SimpleCV.Color.BLUE, 3)
 
-                if x <= 213:
-                    if y <= 160:
-                        speed = SPEED_MULTIPLIER * 5
-                        print "QUAD1: ",
-                    elif y <= 320:
-                        speed = SPEED_MULTIPLIER * 3.5
-                        print "QUAD4: ",
-                    elif y <= 480:
-                        speed = SPEED_MULTIPLIER * 2
-                        print "QUAD7: ",
+                if x < (center_width - percentage_middle):
+                    x_percentage = float(x) / (center_width - percentage_middle)
+                    x_percentage = abs(x_percentage - 100)
+                    left_wheel = -x_percentage * POWER
+                    right_wheel = x_percentage * POWER
+                elif x > (center_width + percentage_middle):
+                    x_percentage = float(x) / (center_width + percentage_middle)
+                    x_percentage = abs(x_percentage - 200)
+                    left_wheel = x_percentage * POWER
+                    right_wheel = -x_percentage * POWER
+                else:
+                    left_wheel = POWER
+                    right_wheel = POWER
 
-                    turn_speed = speed + 4 * SPEED_MULTIPLIER
-                    
-                    if not test:
-                        out_wheels.send((turn_speed, -speed))
-                    print "%d %d" % (speed, turn_speed)
-
-                elif x <= 426:
-                    if y <= 160:
-                        speed = SPEED_MULTIPLIER * 7
-                        print "QUAD2: %d, %d" % (speed, speed)
-                    elif y <= 320:
-                        speed = SPEED_MULTIPLIER * 5
-                        print "QUAD5: %d, %d" % (speed, speed)
-                    elif y <= 480:
-                        speed = 0
-                        print "QUAD8: %d, %d" % (0, 0)
-
-                    if not test:
-                        out_wheels.send((speed, speed))
-                elif x <= 640:
-                    if y <= 160:
-                        speed = SPEED_MULTIPLIER * 5
-                        print "QUAD3: ",
-                    elif y <= 320:
-                        speed = SPEED_MULTIPLIER * 3.5
-                        print "QUAD6: ",
-                    elif y <= 480:
-                        speed = SPEED_MULTIPLIER * 2
-                        print "QUAD9: ",
-
-                    turn_speed = speed + 4 * SPEED_MULTIPLIER
-
-                    if not test:
-                        out_wheels.send((-speed, turn_speed))
-                    print "%d %d" % (turn_speed, speed)
-
-	    else:
-		out_wheels.send((0,0))
+                print "Left %f Right $f" % (left_wheel, right_wheel)
+                out_wheels.send((left_wheel, right_wheel))
+        else:
+            out_wheels.send((0, 0))
 
 if __name__ == "__main__":
     main(None)
