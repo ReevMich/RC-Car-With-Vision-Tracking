58bc9d10e6a873b459ce885072b7fecfdd8c6ec8
dassjosh <dassjosh@my.dom.edu>
2016-04-24 18:49:57 -0500

    comments have been added

--- RaspberryPi/Main_Driver.py
+++ RaspberryPi/Main_Driver.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 
 # Imports
-
 from time import sleep
 from multiprocessing import Process, Pipe
 import DS4_Controller
@@ -13,13 +12,14 @@ import BallTracker
 # Main Method
 def main():
 
-    run_prog = True
+    run_prog = True  # Keeps the program loading
 
-    run_prog_pipe_cntrlr = Pipe()
-    controller_pipe = Pipe()
-    dist_pipe = Pipe()
-    ball_tracker_pipe = Pipe()
+    run_prog_pipe_cntrlr = Pipe()  # Run Program Pipe
+    controller_pipe = Pipe()  # PS4 Controller Pipe
+    dist_pipe = Pipe()  # Distance Sensor Pipe
+    ball_tracker_pipe = Pipe()  # Ball Tracker Pipe
 
+    # Configure All the Processes
     controller_ds4_proc = Process(target=DS4_Controller.main,
                                   args=(controller_pipe, run_prog_pipe_cntrlr))
 
@@ -28,22 +28,26 @@ def main():
                                                             ball_tracker_pipe))
 
     distance_proc = Process(target=Distance_Sensor.main, args=(dist_pipe,))
+
     ball_tracker_proc = Process(target=BallTracker.main,
                                 args=(ball_tracker_pipe,))
 
+    # Run all the processes
     arduino_proc.start()
     controller_ds4_proc.start()
     distance_proc.start()
     ball_tracker_proc.start()
 
+    # Gets the out pipe of run program
     _, in_term_prog = run_prog_pipe_cntrlr
 
+    # Keep running the program until we should terminate
     while run_prog:
         run_prog = in_term_prog.recv()
         print "Program Should terminate:" + str(not run_prog)
-
         sleep(1)
 
+    # Terminate all the processes if the program should terminate
     controller_ds4_proc.terminate()
     distance_proc.terminate()
     arduino_proc.terminate()
